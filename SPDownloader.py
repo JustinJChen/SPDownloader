79a17771810a60d020e27700123c30cce9c7bc8e
justin <justin.jy.chen@utexas.edu>
2020-01-22 14:17:26 -0600

    Able to retrieve deezer track URLs

--- SPDownloader.py
+++ SPDownloader.py
@@ -5,18 +5,22 @@ import json
 URI = '1230626905'
 clientID = 'c1ea3c73f6c54c0296e914eacf59ff78'
 clientSecret = '9cb60e86350544efb91f44f570bcc49a'
-token = 'Bearer BQDc5tGKrQvioWjPyJiFQg3E3I2SAXZNPqzDjFAjfGryMsDH1aXx05yAV3Z2oU1O2aa7snoCpwXpQU43Bd9x3H7wcZdwivcHkUAq0vdfDbX4wOKA9eeDHrmij8lWBpx8jxErxj8O86qa_bziZqSsi5f5NwyJC8Q'
+spotifyToken = 'Bearer BQASL7wgtkqKGrdd8Fy6VM07OKBhO4zswhhU0fgtCGis3ZqpOUwQTg4kAPfMw7RDlcIoG4GrZEZ7zny9bI_LmJ3gOvR2NBoOB--02k-K_FaOYyV5Yya0cPicdZqwz8VUdD5ElFux3ItTxca3dCVr1oUbCelWX-k'
 
-# Get the authorization token
+# Get the authorization token, clientID:clientSecretID need to be base64 encoded
+# Click on URL to get Authorization Token
+# Copy Auth Return token into curl command code
+# Execute curl command to get token variable
+# Execute script
 
 # https://accounts.spotify.com/authorize?client_id=c1ea3c73f6c54c0296e914eacf59ff78&scope=playlist-read-private&response_type=code&redirect_uri=https%3A%2F%2Fgoogle.com%2F
-# curl -H "Authorization: Basic YzFlYTNjNzNmNmM1NGMwMjk2ZTkxNGVhY2Y1OWZmNzg6OWNiNjBlODYzNTA1NDRlZmI5MWY0NGY1NzBiY2M0OWE=" -d grant_type=authorization_code -d code=AQANOeM6B7aS8fZlVVr2Us33cSG6U0N43xQhyh8KVhZLwb5FibyC9sITQZsWNGjMnowf30DrfTdhtAOpgnzwvsI3hPlDCZp5ay-vt22YF-C6zOCHQ3oXngel5S1xD1V1ePVsY4MpUWzhDfTBAzqL2Zf0W29MY_OPo7OeTL6W43l652jssgStWxIKVGqaA3Q7HpjVz1tziF7xYOmmtrRnng -d redirect_uri=https%3A%2F%2Fgoogle.com%2F https://accounts.spotify.com/api/token
+# curl -H "Authorization: Basic YzFlYTNjNzNmNmM1NGMwMjk2ZTkxNGVhY2Y1OWZmNzg6OWNiNjBlODYzNTA1NDRlZmI5MWY0NGY1NzBiY2M0OWE=" -d grant_type=authorization_code -d code=AQCBMQo5jiblxBD_AgSlr7MlsfyNDNJ3hzzrr0m-v6rwRb0js_HFR7GWSnFlnEtRINt96Vbtu_mHJSMlGHPGeY77hQvKW4JHErwM8Vgi4gvnToJXTHkgSokwCwpeIKgB8vw3JIsWuoc7e3Pdh0ddq_d60Igwb1YD1trQ2yb06NgyxxdqJC1vKE49rKMV9XhWtvBGRiO29crxohCXqL9QDQ -d redirect_uri=https%3A%2F%2Fgoogle.com%2F https://accounts.spotify.com/api/token
 
 playlistRequestURL = 'https://api.spotify.com/v1/users/' + URI + '/playlists'
 
 # GET Request getting the playlists from the user
 
-playlistRequest = requests.get(playlistRequestURL, headers={"Authorization": token})
+playlistRequest = requests.get(playlistRequestURL, headers={"Authorization": spotifyToken})
 playlistData = playlistRequest.json()
 playlistID = None
 
@@ -24,24 +28,51 @@ for i in playlistData['items']:
     playlistName = i['name']
     # print ("%s, %s" % (playlistName, playlistID))
 
-    if playlistName == 'edm':
+    if playlistName == 'SPDownloader':
         print(playlistName + "'s ID is: " + i['id'])
         playlistID = i['id']
 
-
-print(playlistID)
 # GET Request getting the track names from the playlist
 
 trackRequestURL = 'https://api.spotify.com/v1/playlists/' + playlistID + '/tracks'
 
-trackRequest = requests.get(trackRequestURL, headers={"Authorization": token})
+trackRequest = requests.get(trackRequestURL, headers={"Authorization": spotifyToken})
 trackData = trackRequest.json()
 trackList = []
 
 for t in trackData['items']:
     trackName = t['track']['name']
     trackList.append(trackName)
-    print(trackName)
+    print(trackName) 
+    # print(trackList)
+
+# Deezer OAuth
+
+# App ID: 391704
+# Secret: 64f056ae7cca43c2b1045b374b74ce71
+# OAuth URL : https://connect.deezer.com/oauth/auth.php?app_id=391704&redirect_uri=https://example.org&perms=basic_access
+
+# Get Access Token by replacing Code with above code: 
+# https://connect.deezer.com/oauth/access_token.php?app_id=391704&secret=64f056ae7cca43c2b1045b374b74ce71&code=fr74a75a8ac3f83966f7c35baa848af0
+
+deezerToken = 'frlOepZxHJVpTcl1LM2TdT29rukfspfA8QEJNf6DPQsDcU3xOFM'
+finalURLs = []
+
+for t in trackList:
+    deezerURL = 'https://api.deezer.com/search?q=' + t
+    deezerRequest = requests.get(deezerURL, headers={"Authorization": deezerToken})
+    deezerData = deezerRequest.json()
+    finalURLs.append(deezerData['data'][0]['link'])
+
+# Create a new file and copy URLs into the text file
+
+file = open("downloadList.txt", "w")
+for t in finalURLs:
+    file.write(t + '\n')
+file.close()
 
 
-print(trackList)
+
+
+
+
