f415677b08488a44280d6ae65df4222d3e88e72a
justin <justin.jy.chen@utexas.edu>
2020-02-05 10:33:47 -0600

    added artists into searching to account for duplicate song names

--- SPDownloader.py
+++ SPDownloader.py
@@ -4,7 +4,7 @@ import json
 URI = '1230626905'
 # clientID = 'c1ea3c73f6c54c0296e914eacf59ff78'
 # clientSecret = '9cb60e86350544efb91f44f570bcc49a'
-spotifyToken = 'Bearer BQASL7wgtkqKGrdd8Fy6VM07OKBhO4zswhhU0fgtCGis3ZqpOUwQTg4kAPfMw7RDlcIoG4GrZEZ7zny9bI_LmJ3gOvR2NBoOB--02k-K_FaOYyV5Yya0cPicdZqwz8VUdD5ElFux3ItTxca3dCVr1oUbCelWX-k'
+spotifyToken = 'Bearer BQBIE2GeWMSoDQXKBdXv-dq13RdN1TeKcb0wIXlLCT-LwDgQ7gP4tqdgBFVCHpuEhd3KitTHJkHGH7tLva4-EDqLSwHNICus8qjF6DCYhXngcMgzAq7LbR29PJA6-TitvvTALMzKmL4-KhdT3rg_pbipxeWQdAI'
 
 # Get the authorization token, clientID:clientSecretID need to be base64 encoded
 # Click on URL to get Authorization Token
@@ -13,7 +13,7 @@ spotifyToken = 'Bearer BQASL7wgtkqKGrdd8Fy6VM07OKBhO4zswhhU0fgtCGis3ZqpOUwQTg4kA
 # Execute script
 
 # https://accounts.spotify.com/authorize?client_id=c1ea3c73f6c54c0296e914eacf59ff78&scope=playlist-read-private&response_type=code&redirect_uri=https%3A%2F%2Fgoogle.com%2F
-# curl -H "Authorization: Basic YzFlYTNjNzNmNmM1NGMwMjk2ZTkxNGVhY2Y1OWZmNzg6OWNiNjBlODYzNTA1NDRlZmI5MWY0NGY1NzBiY2M0OWE=" -d grant_type=authorization_code -d code=AQCBMQo5jiblxBD_AgSlr7MlsfyNDNJ3hzzrr0m-v6rwRb0js_HFR7GWSnFlnEtRINt96Vbtu_mHJSMlGHPGeY77hQvKW4JHErwM8Vgi4gvnToJXTHkgSokwCwpeIKgB8vw3JIsWuoc7e3Pdh0ddq_d60Igwb1YD1trQ2yb06NgyxxdqJC1vKE49rKMV9XhWtvBGRiO29crxohCXqL9QDQ -d redirect_uri=https%3A%2F%2Fgoogle.com%2F https://accounts.spotify.com/api/token
+# curl -H "Authorization: Basic YzFlYTNjNzNmNmM1NGMwMjk2ZTkxNGVhY2Y1OWZmNzg6OWNiNjBlODYzNTA1NDRlZmI5MWY0NGY1NzBiY2M0OWE=" -d grant_type=authorization_code -d code=AQCZf9iHleoaHhynpIBVlVaS8iJqlfkxv80q0KgMpH0eJS_yx72uSUwV_arM3Fau9pTGDb5i-GAHnd--FVk6we190_cNrTLkxfSFQLtrUX-tU61GJPF5RPlJZWrlKKbF6QtGV-Kriul3cTikvtOFKRaOdpftt7Ivx2ouPbsZNHTUz9VgwSE2XsCuRIRcZWNhtddFDSWK0gMAlav31CEQmw -d redirect_uri=https%3A%2F%2Fgoogle.com%2F https://accounts.spotify.com/api/token
 
 playlistRequestURL = 'https://api.spotify.com/v1/users/' + URI + '/playlists'
 
@@ -33,13 +33,15 @@ for i in playlistData['items']:
 trackRequestURL = 'https://api.spotify.com/v1/playlists/' + playlistID + '/tracks'
 
 trackRequest = requests.get(trackRequestURL, headers={"Authorization": spotifyToken})
-trackData = trackRequest.json()
+trackData = trackRequest.json() 
+print(trackData)
 trackList = []
 
 for t in trackData['items']:
     trackName = t['track']['name']
-    trackList.append(trackName)
-    print(trackName) 
+    trackArtist = t['track']['artists'][0]['name']
+    trackList.append(trackName + ' ' + trackArtist)
+    print(trackName + ' ' + trackArtist) 
 
 # Deezer OAuth
 
@@ -48,10 +50,10 @@ for t in trackData['items']:
 # OAuth URL : https://connect.deezer.com/oauth/auth.php?app_id=391704&redirect_uri=https://example.org&perms=basic_access
 
 # Get Access Token by replacing Code with above code: 
-# https://connect.deezer.com/oauth/access_token.php?app_id=391704&secret=64f056ae7cca43c2b1045b374b74ce71&code=fr74a75a8ac3f83966f7c35baa848af0
+# https://connect.deezer.com/oauth/access_token.php?app_id=391704&secret=64f056ae7cca43c2b1045b374b74ce71&code=frb7a137de9a8738e81df78d763fe358
 
 # Update deezer token with new access token
-deezerToken = 'frlOepZxHJVpTcl1LM2TdT29rukfspfA8QEJNf6DPQsDcU3xOFM'
+deezerToken = 'frNO8y6MbeWpDZICI98CRv7FIg2cGJTTVD1j0dLYeumNiqlxZ9w'
 finalURLs = []
 
 for t in trackList:
